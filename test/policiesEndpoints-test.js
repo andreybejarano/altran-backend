59c85005743ee3dbd38fddeea41318bfd95d152c
andreybejarano <andreybejarano@gmail.com>
2017-10-18 22:19:00 -0300

    feat: add routes and controller

---
+++ test/policiesEndpoints-test.js
@@ -0,0 +1,26 @@
+'use strict';
+const test = require('ava');
+const request = require('request-promise');
+const config = require('../lib/config');
+
+test('GET /policies/username/:username', async t => {
+	let options = {
+		method: 'GET',
+		uri: `http://localhost:${config.port}/policies/username/Britney`,
+		json: true
+	};
+
+	let user = await request(options);
+	t.is(typeof user, 'object');
+});
+
+test('GET /policies/:id/user', async t => {
+	let options = {
+		method: 'GET',
+		uri: `http://localhost:${config.port}/policies/64cceef9-3a01-49ae-a23b-3761b604800b/user`,
+		json: true
+	};
+
+	let user = await request(options);
+	t.is(typeof user, 'object');
+});
---
+++ test/usersEndpoints-test.js
@@ -0,0 +1,26 @@
+'use strict';
+const test = require('ava');
+const request = require('request-promise');
+const config = require('../lib/config');
+
+test('GET /users/:id', async t => {
+	let options = {
+		method: 'GET',
+		uri: `http://localhost:${config.port}/users/a0ece5db-cd14-4f21-812f-966633e7be86`,
+		json: true
+	};
+
+	let user = await request(options);
+	t.is(typeof user, 'object');
+});
+
+test('GET /users/name/:name', async t => {
+	let options = {
+		method: 'GET',
+		uri: `http://localhost:${config.port}/users/name/Britney`,
+		json: true
+	};
+
+	let user = await request(options);
+	t.is(typeof user, 'object');
+});
